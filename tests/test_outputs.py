377cfcd30eb50ea66ca7323c7d1513b357dec983
alahirajeffrey <alahirajeffrey@gmail.com>
2026-01-17 00:46:04 -1200

    commit

---
+++ tests/test_outputs.py
@@ -0,0 +1,114 @@
+import logging
+from solution.logger import setup_logger
+import json
+from opentelemetry import trace
+from solution.tracer import setup_tracing
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+"""Tests to validate logs."""
+
+## SHOULD PASS: test for X-Correlation-ID in response headers
+def test_correlation_id_header(client):
+    response = client.get("/metrics")
+    assert "X-Correlation-ID" in response.headers
+    correlation_id = response.headers["X-Correlation-ID"]
+    assert correlation_id.strip() != ""
+
+
+## SHOULD PASS: test to ensure logger level is info
+def test_logger_level_is_info():
+    setup_logger()
+    logger = logging.getLogger()
+    assert logger.level == logging.INFO
+
+
+## SHOULD FAIL: test to check if logger level is error
+def test_logger_level_is_error():
+    setup_logger()
+    logger = logging.getLogger()
+    assert logger.level == logging.ERROR
+
+
+## SHOULD PASS: test to check if log is in json format
+def test_logger_formatter_is_json(caplog):
+    setup_logger()
+    logger = logging.getLogger()
+
+    with caplog.at_level(logging.INFO):
+        logger.info('{"message": "testing"}')
+
+    # get log
+    log_data = caplog.records[0].getMessage()
+
+    # parse log record to json
+    try:
+        log_json = json.loads(log_data)
+    except json.JSONDecodeError:
+        log_json = None
+
+    assert log_json is not None
+
+## SHOULD PASS: test to ensure at least one StreamHandler is attached
+def test_logger_has_stream_handler():
+    setup_logger()
+    logger = logging.getLogger()
+    assert any(isinstance(h, logging.StreamHandler) for h in logger.handlers)
+
+"""Tests metrics are being exposed."""
+
+## SHOULD PASS: test to ensure total http request is in metrics response
+def test_http_request_total(client):
+    response = client.get("/metrics")
+    assert b"http_requests_total" in response.data
+
+
+## SHOULD PASS: test to ensure http request latency in seconds is in metrics response
+def test_http_request_latency_seconds(client):
+    response = client.get("/metrics")
+    assert b"http_request_latency_seconds" in response.data
+
+
+## SHOULD PASS: test to ensure total requests per second is in metrics response
+def test_requests_per_second(client):
+    response = client.get("/metrics")
+    assert b"http_requests_per_second" in response.data
+
+
+## SHOULD PASS: test to ensure metrics endpoint returns a 200 response code
+def test_metrics_include_status_codes(client):
+    response = client.get("/metrics")
+    assert response.status_code == 200
+
+
+## SHOULD FAIL: test to ensure http request per hour is not in metrics response
+def test_http_request_per_hour(client):
+    response = client.get("/metrics")
+    assert b"http_request_per_hour" in response.data
+
+
+
+
+"""Tests to check if traces are being sent."""
+
+## SHOULD PASS: test to see if tracer is configured
+def test_tracer_is_configured():
+    tracer = trace.get_tracer(__name__)
+    assert tracer is not None
+
+
+## SHOULD FAIL: test to see if tracer is not configured
+def test_tracer_provider_is_none():
+    provider = trace.get_tracer_provider()
+    assert provider is None
+
+
+## SHOULD PASS: test to see if tracer is an instance of trace provier
+def test_tracer_provider_is_set(client):
+    setup_tracing(client.application)
+
+    ## get the global tracer provider
+    provider = trace.get_tracer_provider()
+    assert provider is not None
+    from opentelemetry.sdk.trace import TracerProvider
+
+    assert isinstance(provider, TracerProvider)
